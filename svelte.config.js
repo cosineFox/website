170673ed358a2e3d7cf2b962eba976465a391df4
cosineFox <kytzukitsune@gmail.com>
2026-01-02 04:34:14 +0800

    Migrate deployment to Cloudflare Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,10 +6,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			out: 'build',
-			precompress: true
-		}),
+		adapter: adapter(),
 		// Prerender static pages for better performance
 		prerender: {
 			entries: ['/main']
